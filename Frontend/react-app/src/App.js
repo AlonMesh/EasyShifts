f51b5c349c16dfa3363afafba9989855a2d06e6b
NetaCohen4 <netaco432@gmail.com>
2024-02-19 22:25:27 +0200

    react files added

---
+++ Frontend/react-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WebSocketClient from "./components/WebSocketClient.js";
+
+function App() {
+  return (
+    <div className="App">
+      <WebSocketClient />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/react-app/src/components/WebSocketClient.js
@@ -0,0 +1,27 @@
+// WebSocketClient.js
+
+
+import './WebSocketClient.css'; // Import your CSS file if you have one
+
+class WebSocketClient extends React.Component {
+  redirectToLogin = () => {
+    //login component
+  };
+
+  redirectToSignUp = () => {
+    //sign up component
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Easy Shifts!</h1>
+        <button onClick={this.redirectToLogin}>Login</button>
+        <button onClick={this.redirectToSignUp}>Sign Up</button>
+        <div id="log"></div>
+      </div>
+    );
+  }
+}
+
+export default WebSocketClient;
