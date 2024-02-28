353b119d34da4e1f4d48ddd434a0470846a70fd9
shovshi <shovalnh12@gmail.com>
2024-02-28 15:52:52 +0200

    Fixed warnings

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/App'; // Assuming your root component is named App and located in App.js
+import ReactDOM from 'react-dom/client';
+import App from './App/App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
