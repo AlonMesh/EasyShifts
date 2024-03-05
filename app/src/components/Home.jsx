e709c231a15b1dae30c7e357a11a8d7d26486acf
NetaCohen4 <netaco432@gmail.com>
2024-03-05 11:26:29 +0200

    Toolbar added

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
+      <h1>Welcome to Easy Shifts!</h1>
       <button><Link to="/login">Login</Link></button>
       <button><Link to="/signup">Sign Up</Link></button>
     </div>
